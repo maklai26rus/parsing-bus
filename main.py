d5b960f6ab33cdba8943fd62d7446a8f42e8b297
Maklai <maklai26rus@mai.ru>
2021-11-30 22:00:51 +0300

    начинаю тесты

--- main.py
+++ main.py
@@ -20,7 +20,6 @@ def get_url(url):
         response = requests.get(url_page)
         soup = BeautifulSoup(response.text, "lxml")
 
-        # flight_items = soup.find('table', class_='views-table cols-9').find_all('tr')
         flight_items = soup.find_all("td", class_="views-field views-field-path")
         for fi in flight_items:
             links_page = fi.find("a").get('href')
@@ -28,75 +27,30 @@ def get_url(url):
             url_bilet = "https://ros-bilet.ru/reys/yalta/novocherkassk-5124586"
             response = requests.get(url_bilet)
             soup = BeautifulSoup(response.text, "lxml")
-            otkyda = soup.find('div', class_='bus-stantion-info-text').text
-            print(otkyda)
+            road_bilet = soup.find_all('div', class_='bus-stantion-info-text')
 
+            start_bilet = road_bilet[0].text.split(',')[0]
+            end_bilet = road_bilet[1].text.split(',')[0]
 
-            # road = soup.find('h1')
-            # field_item_even = soup.find_all('div', class_="field-item even")
-            # start_time = field_item_even[0].text.strip()
-            # end_time = field_item_even[1].text.strip()
-            # price_adult = field_item_even[4].text.strip()
-            # price_children = field_item_even[5].text.strip()
+            field_item_even = soup.find_all('div', class_="field-item even")
 
-            # time_default = soup.find_all('div', class_="time-default")
-            # start_time = time_default[0].text.strip()
-            # end_time = time_default[1].text.strip()
-            #TODO не могу определить время в пути
+            price_adult = field_item_even[4].text.strip()
+            price_children = field_item_even[5].text.strip()
+
+            time_default = soup.find_all('div', class_="time-default")
+            start_time = time_default[0].text.strip()
+            end_time = time_default[1].text.strip()
+            # TODO не могу определить время в пути
             # travel_time = soup.find_all('div', class_="tline even")
             # print(travel_time[0].find_all('div', class_="time-default"))
 
-            # start_road = field_item_even[6].text.split(',')[0]
-            # end_road = field_item_even[6].text.split(',')[-1]
+            start_road = field_item_even[6].text.split(',')[0]
+            end_road = field_item_even[6].text.split(',')[-1]
 
+            print(start_bilet, '-', end_bilet, start_time, end_time, price_adult, '/', price_children, start_road, '-',
+                  end_road)
 
             break
-            # flight_date = fi.find_all("td")
-
-            # print(flight_date)
-            # try:
-            #     flight_nember = flight_date[8].text.strip()
-            # except:
-            #     flight_nember = 'NO'
-            # print(flight_nember)
-            # try:
-            #     flight_nember = flight_date[0].text.strip()
-            # except:
-            #     flight_nember = 'NO'
-            # try:
-            #     beginning_route = flight_date[1].text.rstrip()
-            # except:
-            #     beginning_route = 'NO'
-            #
-            # try:
-            #     end_route = flight_date[3].text.strip()
-            # except:
-            #     end_route = 'NO'
-            #
-            # try:
-            #     sending_time = flight_date[2].text.strip()
-            # except:
-            #     sending_time = 'NO'
-            #
-            # try:
-            #     arrival_time = flight_date[4].text.strip()
-            # except:
-            #     arrival_time = 'NO'
-            #
-            # try:
-            #     bus = flight_date[5].text.strip()
-            # except:
-            #     bus = 'NO'
-            #
-            # try:
-            #     money = flight_date[6].text.rstrip('\n')
-            # except:
-            #     money = 'NO'
-
-            # list_bus.append((flight_nember, beginning_route, end_route, sending_time, arrival_time, bus, money))
-    # with open('chek.txt', "a", encoding="utf-8") as file:
-    #     for line in list_bus:
-    #         file.write(f"{line}\n")
 
 
 def main():
